c0cebefe271a09fdc5f35edfc936fcb56a97dadb
Aaron Tomb <aarontomb@gmail.com>
2012-04-01 20:58:47 -0700

    Mimic almost all of dexdump.
    Debug information and exception handling information are missing. Method
    type descriptors are wrong.

--- Dexdumper.hs
+++ Dexdumper.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as UTF8
 import qualified Data.Map as Map
+import Data.Word
 import System.Environment
 import Text.Printf
 
@@ -21,13 +22,28 @@ processFile f = do
       mapM_ putStrLn . clsLines . dexClasses $ dex
         where clsLines = concat . map (classLines dex) . Map.toList
 
+pSDI :: StringDataItem -> String
+pSDI (SDI _ octs) = UTF8.toString (BS.pack octs)
+
+pSDI' :: StringDataItem -> String
+pSDI' (SDI _ octs) = squotes . UTF8.toString . BS.pack $ octs
+
+escape :: String -> String
+escape [] = []
+escape ('\0' : s) = '\\' : '0' : escape s
+escape ('\n' : s) = '\\' : 'n' : escape s
+escape (c : s) = c : escape s
+
+squotes :: String -> String
+squotes s = "'" ++ s ++ "'"
+
 hdrLines :: FilePath -> DexHeader -> [String]
 hdrLines f hdr =
-  [ printf "Opened '%s', DEX version %s"
-    f (show (dexVersion hdr))
+  [ printf "Opened '%s', DEX version '%s'"
+    f (escape (UTF8.toString (BS.take 3 (dexVersion hdr))))
   , "DEX file header:"
-  , fld "magic" $
-    show (BS.append (dexMagic hdr) (dexVersion hdr))
+  , fld "magic" $ squotes $ escape $
+    UTF8.toString (BS.append (dexMagic hdr) (dexVersion hdr))
   , fld "checksum" . h8 . dexChecksum $ hdr
   , fld "signature" $ sig (dexSHA1 hdr)
   , fldn "file_size" $ dexFileLen hdr
@@ -53,12 +69,42 @@ hdrLines f hdr =
 
 fld :: String -> String -> String
 fld = printf "%-20s: %s"
+
+fldx :: String -> Word32 -> String
 fldx n v = fld n (printf "%d (0x%06x)" v v)
+
+fldx4 :: String -> Word32 -> String
 fldx4 n v = fld n (printf "%d (0x%04x)" v v)
+
+fldn :: (Show a) => String -> a -> String
 fldn n v = fld n (show v)
-fldns n v s = fld n (printf "%04x (%s)" v s)
-getStr i dex = Map.findWithDefault (SDI 0 []) i (dexStrings dex)
-getTypeName i dex = Map.findWithDefault 0 i (dexTypeNames dex)
+
+fldxs :: String -> Word32 -> String -> String
+fldxs n v s = fld n (printf "0x%04x (%s)" v s)
+
+fldns :: String -> Word32 -> String -> String
+fldns n v s = fld n (printf "%d (%s)" v s)
+
+getStr :: DexFile -> StringId -> StringDataItem
+getStr dex i = Map.findWithDefault (error msg) i (dexStrings dex)
+  where msg = printf "Unknown string ID %d" i
+
+getTypeName :: DexFile -> TypeId -> StringDataItem
+getTypeName dex i =
+  getStr dex (Map.findWithDefault (error msg) i (dexTypeNames dex))
+    where msg = printf "Uknown type ID %d" i
+
+getField :: DexFile -> FieldId -> Field
+getField dex i = Map.findWithDefault (error msg) i (dexFields dex)
+  where msg = printf "Unknown field ID %d" i
+
+getMethod :: DexFile -> MethodId -> Method
+getMethod dex i = Map.findWithDefault (error msg) i (dexMethods dex)
+  where msg = printf "Unknown method ID %d" i
+
+getProto :: DexFile -> ProtoId -> Proto
+getProto dex i = Map.findWithDefault (error msg) i (dexProtos dex)
+  where msg = printf "Unknown prototype ID %d" i
 
 classLines :: DexFile -> (TypeId, Class) -> [String]
 classLines dex (i, cls) =
@@ -76,11 +122,65 @@ classLines dex (i, cls) =
   , fldn "direct_methods_size" $ length (classDirectMethods cls)
   , fldn "virtual_methods_size" $ length (classVirtualMethods cls)
   , ""
-  , printf "Class #%-13d-" i
-  , fld "  Class descriptor" $ showSDI $
-    getStr (getTypeName (classId cls) dex) dex
-  , fldns "  Access flags"
+  , printf "Class #%d            -" i
+  , fld "  Class descriptor" $ pSDI' $ getTypeName dex (classId cls)
+  , fldxs "  Access flags"
     (classAccessFlags cls) (flagsString AClass (classAccessFlags cls))
+  , fld "  Superclass" $ pSDI' $ getTypeName dex (classSuperId cls)
+  , "  Interfaces        -"
+  ]
+  ++ concatMap (interfaceLines dex) (zip [0..] (classInterfaces cls)) ++
+  [ "  Static fields     -" ]
+  ++ concatMap (fieldLines dex) (zip [0..] (classStaticFields cls)) ++
+  [ "  Instance fields   -" ]
+  ++ concatMap (fieldLines dex) (zip [0..] (classInstanceFields cls)) ++
+  [ "  Direct methods    -" ]
+  ++ concatMap (methodLines dex) (zip [0..] (classDirectMethods cls)) ++
+  [ "  Virtual methods   -" ]
+  ++ concatMap (methodLines dex) (zip [0..] (classVirtualMethods cls)) ++
+  [ fldns "  source_file_idx"
+    (classSourceNameId cls)
+    (pSDI (getStr dex (classSourceNameId cls)))
+  ]
+
+interfaceLines :: DexFile -> (Word32, TypeId) -> [String]
+interfaceLines dex (n, i) =
+  [ fld (printf "    #%d" n) (pSDI' (getTypeName dex i)) ]
+
+fieldLines :: DexFile -> (Word32, EncodedField) -> [String]
+fieldLines dex (n, f) =
+  [ printf "    #%d              : (in %s)" n clsName
+  , fld "      name" . pSDI' . getStr dex . fieldNameId $ field
+  , fld "      type" . pSDI' . getTypeName dex . fieldTypeId $ field
+  , fldxs "      access"
+    (fieldAccessFlags f)
+    (flagsString AField (fieldAccessFlags f))
+  ] where field = getField dex i
+          clsName = pSDI . getTypeName dex . fieldClassId $ field
+          i = fieldId f
+
+methodLines :: DexFile -> (Word32, EncodedMethod) -> [String]
+methodLines dex (n, m) =
+  [ printf "    #%d              : (in %s)" n clsName
+  , fld "      name" . pSDI' . getStr dex . methNameId $ method
+  , fld "      type" . pSDI' . getStr dex . protoDesc $ proto
+  , fldxs "      access"
+    (methAccessFlags m)
+    (flagsString AMethod (methAccessFlags m))
+  ] ++ maybe [] (codeLines dex) (methCode m)
+    where method = getMethod dex i
+          proto = getProto dex (methProtoId method)
+          clsName = pSDI . getTypeName dex . methClassId $ method
+          i = methId m
+
+codeLines :: DexFile -> CodeItem -> [String]
+codeLines dex code =
+  [ "      code          -"
+  , fldn "      registers" $ codeRegs code
+  , fldn "      ins" $ codeInSize code
+  , fldn "      outs" $ codeOutSize code
+  , fld "      insns size" $
+    printf "%d 16-bit code units" (length (codeInsns code))
   ]
 
 main :: IO ()
