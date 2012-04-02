f21e26e36efd3d8d2de0e09103ef5cc916cca760
Aaron Tomb <aarontomb@gmail.com>
2012-04-01 20:58:21 -0700

    Update treatment of access flags.

--- Dalvik/AccessFlags.hs
+++ Dalvik/AccessFlags.hs
@@ -56,35 +56,61 @@ tyString AClass = "class"
 tyString AField = "field"
 tyString AMethod = "method"
 
-flagString :: AccessType -> AccessFlag -> String
-flagString _ ACC_PUBLIC = "PUBLIC"
-flagString _ ACC_PRIVATE = "PRIVATE"
-flagString _ ACC_PROTECTED = "PROTECTED"
-flagString _ ACC_STATIC = "STATIC"
-flagString _ ACC_FINAL = "FINAL"
-flagString AMethod ACC_SYNCHRONIZED = "SYNCHRONIZED"
-flagString AField  ACC_VOLATILE = "VOLATILE"
-flagString AMethod ACC_BRIDGE = "BRIDGE"
-flagString AField  ACC_TRANSIENT = "TRANSIENT"
-flagString AMethod ACC_VARARGS = "VARARGS"
-flagString AMethod ACC_NATIVE = "NATIVE"
-flagString AClass  ACC_INTERFACE = "INTERFACE"
-flagString AClass ACC_ABSTRACT = "ABSTRACT"
-flagString AMethod ACC_ABSTRACT = "ABSTRACT"
-flagString AMethod ACC_STRICT = "STRICT"
-flagString _ ACC_SYNTHETIC = "SYNTHETIC"
-flagString AClass  ACC_ANNOTATION = "ANNOTATION"
-flagString AClass ACC_ENUM = "ENUM"
-flagString AField ACC_ENUM = "ENUM"
-flagString AMethod ACC_CONSTRUCTOR = "CONSTRUCTOR"
-flagString AMethod ACC_DECLARED_SYNCHRONIZED = "DECLARED_SYNCHRONIZED"
-flagString ty f =
-  printf "(unknown access flag %08x for %s)" (flagCode f) (tyString ty)
+codeFlag :: AccessType -> Word32 -> AccessFlag
+codeFlag _       0x00001 = ACC_PUBLIC
+codeFlag _       0x00002 = ACC_PRIVATE
+codeFlag _       0x00004 = ACC_PROTECTED
+codeFlag _       0x00008 = ACC_STATIC
+codeFlag _       0x00010 = ACC_FINAL
+codeFlag AMethod 0x00020 = ACC_SYNCHRONIZED
+codeFlag AField  0x00040 = ACC_VOLATILE
+codeFlag AMethod 0x00040 = ACC_BRIDGE
+codeFlag AField  0x00080 = ACC_TRANSIENT
+codeFlag AMethod 0x00080 = ACC_VARARGS
+codeFlag AMethod 0x00100 = ACC_NATIVE
+codeFlag AClass  0x00200 = ACC_INTERFACE
+codeFlag AClass  0x00400 = ACC_ABSTRACT
+codeFlag AMethod 0x00400 = ACC_ABSTRACT
+codeFlag AMethod 0x00800 = ACC_STRICT
+codeFlag _       0x01000 = ACC_SYNTHETIC
+codeFlag AClass  0x02000 = ACC_ANNOTATION
+codeFlag AClass  0x04000 = ACC_ENUM
+codeFlag AField  0x04000 = ACC_ENUM
+codeFlag AMethod 0x10000 = ACC_CONSTRUCTOR
+codeFlag AMethod 0x20000 = ACC_DECLARED_SYNCHRONIZED
+codeFlag ty bits =
+  error $ printf "(unknown access flag %08x for %s)" bits (tyString ty)
+
+flagString :: AccessFlag -> String
+flagString ACC_PUBLIC = "PUBLIC"
+flagString ACC_PRIVATE = "PRIVATE"
+flagString ACC_PROTECTED = "PROTECTED"
+flagString ACC_STATIC = "STATIC"
+flagString ACC_FINAL = "FINAL"
+flagString ACC_SYNCHRONIZED = "SYNCHRONIZED"
+flagString ACC_VOLATILE = "VOLATILE"
+flagString ACC_BRIDGE = "BRIDGE"
+flagString ACC_TRANSIENT = "TRANSIENT"
+flagString ACC_VARARGS = "VARARGS"
+flagString ACC_NATIVE = "NATIVE"
+flagString ACC_INTERFACE = "INTERFACE"
+flagString ACC_ABSTRACT = "ABSTRACT"
+flagString ACC_STRICT = "STRICT"
+flagString ACC_SYNTHETIC = "SYNTHETIC"
+flagString ACC_ANNOTATION = "ANNOTATION"
+flagString ACC_ENUM = "ENUM"
+flagString ACC_CONSTRUCTOR = "CONSTRUCTOR"
+flagString ACC_DECLARED_SYNCHRONIZED = "DECLARED_SYNCHRONIZED"
 
 flagsString :: AccessType -> Word32 -> String
 flagsString ty w =
-  unwords [ flagString ty f | f <- allFlags, w .&. (flagCode f) /= 0 ]
-    where allFlags = [ACC_PUBLIC .. ACC_DECLARED_SYNCHRONIZED]
+  unwords [ flagString (codeFlag ty c) | c <- allCodes, w .&. c /= 0 ]
+    where allCodes = [ 0x00001, 0x00002, 0x00004, 0x00008
+                     , 0x00010, 0x00020, 0x00040, 0x00080
+                     , 0x00100, 0x00200, 0x00400, 0x00800
+                     , 0x01000, 0x02000, 0x04000
+                     , 0x10000, 0x20000
+                     ]
 
 andTrue :: Word32 -> Word32 -> Bool
 andTrue w1 w2 = (w1 .&. w2) /= 0
